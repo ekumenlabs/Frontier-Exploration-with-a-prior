3d592ce551db6ea5c2e44e77dadeadae5534f2e9
Agustin Mazzeo <agustin.mazzeo@rocketinsights.com>
2022-07-23 18:54:24 -0300

    CodeRefactor: - feat: Refactored approeaches code - feat: Updated notebooks to use local code - feat: Saving read pkl files

--- src/FrontierExploration/preprocessing/trajectory/naive_approach.py
+++ src/FrontierExploration/preprocessing/trajectory/naive_approach.py
@@ -4,12 +4,18 @@ from copy import deepcopy
 import matplotlib.pyplot as plt
 import numpy as np
 from numpy import hypot
+from itertools import tee
 
 from FrontierExploration.preprocessing.grid.occupancy_grid import CELL_VALUES, OCCUPIED, OccupancyGrid, is_cell_empty, VIEWED, EMPTY
 from FrontierExploration.preprocessing.grid.mock_grid import create_mock_grid
 from FrontierExploration.preprocessing.grid.raycasting import raycast_in_every_direction
 from FrontierExploration.preprocessing.trajectory.astar import AStar
 
+def pairwise(iterable):
+    "s -> (s0,s1), (s1,s2), (s2, s3), ..."
+    a, b = tee(iterable)
+    next(b, None)
+    return zip(a, b)
 
 def paginate(lst: List[any], n: int):
     for i in range(0, len(lst), n):
@@ -35,50 +41,72 @@ class NaiveFrontierExploration(object):
         """
         cell_to_evaluate = self._start_cell
         ret = []
-        frontier_cells = []
-        cells_to_explore = len(self._cells_to_view)
 
+        cells_to_evaluate = self._get_cells_to_evaluate()
+
+        for cell_to_evaluate, next_cell_to_evaluate in pairwise(cells_to_evaluate):
+            # run astar to find the path between current point and chosen frontier point
+            ret.extend(self._astar.solve(start_cell=cell_to_evaluate, end_cell=next_cell_to_evaluate))
+            # reset the astar algorithm
+            self._astar.reset()
+                  
+        return ret
+
+    @property
+    def cells_to_view(self) -> int:
+        return len(self._cells_to_view)
+
+    def _get_cells_to_evaluate(self):
+        cell_to_evaluate = self._start_cell
+        cells_to_evaluate = [self._start_cell]
+        frontier_cells = []
+        cells_to_explore = self.cells_to_view
         with tqdm.tqdm(total=cells_to_explore) as pbar:
-            while len(self._cells_to_view) > 1:
-                last_cells_to_view = len(self._cells_to_view)
+            while self.cells_to_view > 1:
+                last_cells_to_view = self.cells_to_view
                 # check which cells you can view from the current cell
-                raycasted_cells = self._raycast_in_every_direction(
-                    cell=cell_to_evaluate, available_cells=self._cells_to_view)
-                # mark them as viewed, and check whether they're frontiers or not (frontier == boundary between explored and unexplored)
-                for cell in raycasted_cells:
-                    self._grid[tuple(cell)] = VIEWED
-                    if self._is_frontier(tuple(cell)):
-                        frontier_cells.append(tuple(cell))
-                # update the progress bar
+                frontier_cells.extend(self._get_frontier_cells(cell_to_evaluate))
                 self._grid[cell_to_evaluate] = VIEWED
                 self._cells_to_view = self._grid.get_empty_cells()
-                pbar.update(last_cells_to_view - len(self._cells_to_view))
-                last_cells_to_view = len(self._cells_to_view)
+
+                # update the progress bar
+                pbar.update(last_cells_to_view - self.cells_to_view)
+
                 # pick the next cell to navigate to (closest frontier)
-                next_cell_to_evaluate = None
-                best_distance = None
-                for cell in frontier_cells:
-                    cell_distance = cell_distance = distance(cell, cell_to_evaluate)
-                    if best_distance is None or cell_distance < best_distance:
-                        next_cell_to_evaluate = cell
-                        best_distance = cell_distance
-                # run astar to find the path between current point and chosen frontier point
-                ret.extend(self._astar.solve(
-                    start_cell=cell_to_evaluate, end_cell=next_cell_to_evaluate))
-                # reset the astar algorithm
-                self._astar.reset()
+                next_cell_to_evaluate = self._get_next_cell_to_evaluate(cell_to_evaluate, frontier_cells)
+                if next_cell_to_evaluate is None:
+                    break
+                cells_to_evaluate.append(next_cell_to_evaluate)
+                # with the freshly viewed cells, maybe some old frontier cells are no longer frontiers, revisit.
+                [frontier_cells.remove(cell) for cell in frontier_cells if not self._is_frontier(cell)]
                 cell_to_evaluate = next_cell_to_evaluate
                 # mark the cell you're going to navigate as empty, so the subsequent calls don't crash
                 self._grid[cell_to_evaluate] = EMPTY
                 self._cells_to_view = self._grid.get_empty_cells()
-                # with the freshly viewed cells, maybe some old frontier cells are no longer frontiers, revisit.
-                frontiers_to_delete = []
-                for cell in frontier_cells:
-                    if not self._is_frontier(cell):
-                        frontiers_to_delete.append(cell)
-                for cell in frontiers_to_delete:
-                    frontier_cells.remove(cell)
-        return ret
+        return cells_to_evaluate
+
+
+    def _get_frontier_cells(self, cell_to_evaluate):
+        frontier_cells = []
+        # check which cells you can view from the current cell
+        raycasted_cells = self._raycast_in_every_direction(
+            cell=cell_to_evaluate, available_cells=self._cells_to_view)
+        # mark them as viewed, and check whether they're frontiers or not (frontier == boundary between explored and unexplored)
+        for cell in raycasted_cells:
+            self._grid[tuple(cell)] = VIEWED
+            if self._is_frontier(tuple(cell)):
+                frontier_cells.append(tuple(cell))
+        return frontier_cells
+
+    def _get_next_cell_to_evaluate(self, cell_to_evaluate, frontier_cells):
+        next_cell_to_evaluate = None
+        best_distance = None
+        for cell in frontier_cells:
+            cell_distance = cell_distance = distance(cell, cell_to_evaluate)
+            if best_distance is None or cell_distance < best_distance:
+                next_cell_to_evaluate = cell
+                best_distance = cell_distance
+        return next_cell_to_evaluate
 
     def _is_frontier(self, cell: tuple) -> bool:
         """
@@ -93,8 +121,8 @@ class NaiveFrontierExploration(object):
         lidar_range = self._range_in_cells if not frontier_eval else self._range_in_cells
         return raycast_in_every_direction(available_cells=available_cells, start_cell=cell, range_in_cells=lidar_range, squarify=True)
 
-    def plot(self):
-        return self._grid.plot()
+    def plot(self, *args, **kwargs):
+        return self._grid.plot(*args, **kwargs)
 
 
 if __name__ == "__main__":
--- src/FrontierExploration/preprocessing/trajectory/not_naive_approach.py
+++ src/FrontierExploration/preprocessing/trajectory/not_naive_approach.py
@@ -1,4 +1,5 @@
 from copy import deepcopy
+from itertools import tee
 from math import hypot
 import matplotlib.pyplot as plt
 import numpy as np
@@ -10,6 +11,11 @@ from FrontierExploration.preprocessing.trajectory.astar import AStar
 from FrontierExploration.preprocessing.grid.mock_grid import create_mock_grid
 from FrontierExploration.preprocessing.grid.occupancy_grid import CELL_VALUES, OCCUPIED, OccupancyGrid, is_cell_empty, VIEWED, EMPTY
 
+def pairwise(iterable):
+    "s -> (s0,s1), (s1,s2), (s2, s3), ..."
+    a, b = tee(iterable)
+    next(b, None)
+    return zip(a, b)
 
 def paginate(lst: List[any], n: int):
     for i in range(0, len(lst), n):
@@ -39,52 +45,45 @@ class BlueprintAwareFrontierExploration(object):
         """
         cell_to_evaluate = self._start_cell
         ret = []
-        frontier_cells = []
-        cells_to_explore = len(self._cells_to_view)
 
+        cells_to_evaluate = self._get_cells_to_evaluate()
+
+        for cell_to_evaluate, next_cell_to_evaluate in pairwise(cells_to_evaluate):
+            # run astar to find the path between current point and chosen frontier point
+            ret.extend(self._astar.solve(start_cell=cell_to_evaluate, end_cell=next_cell_to_evaluate))
+            # reset the astar algorithm
+            self._astar.reset()
+                  
+        return ret
+
+    def _get_cells_to_evaluate(self):
+        cell_to_evaluate = self._start_cell
+        cells_to_evaluate = [self._start_cell]
+        frontier_cells = []
+        cells_to_explore = self.cells_to_view
         with tqdm.tqdm(total=cells_to_explore) as pbar:
-            while len(self._cells_to_view) > 1:
-                last_cells_to_view = len(self._cells_to_view)
+            while self.cells_to_view > 1:
+                last_cells_to_view = self.cells_to_view
                 # check which cells you can view from the current cell
-                raycasted_cells = self._raycast_in_every_direction(
-                    cell=cell_to_evaluate, available_cells=self._original_empty_cells)
-                # mark them as viewed, and check whether they're frontiers or not (frontier == boundary between explored and unexplored)
-                for cell in raycasted_cells:
-                    self._grid[tuple(cell)] = VIEWED
-                    if self._is_frontier(tuple(cell)):
-                        frontier_cells.append(tuple(cell))
-                # update the progress bar
+                frontier_cells.extend(self._get_frontier_cells(cell_to_evaluate))
                 self._grid[cell_to_evaluate] = VIEWED
                 self._cells_to_view = self._grid.get_empty_cells()
-                pbar.update(last_cells_to_view - len(self._cells_to_view))
-                last_cells_to_view = len(self._cells_to_view)
-                next_cell_to_evaluate = None
-                best_visibility = None
-                    
-                self._raycast_some(cells=frontier_cells)
-                for cell in frontier_cells:
-                    if best_visibility is None or len(self._visibility_grid[cell] > best_visibility):
-                        next_cell_to_evaluate = cell
-                        best_visibility = len(self._visibility_grid[cell])
 
-                    
-                # run astar to find the path between current point and chosen frontier point
-                ret.extend(self._astar.solve(
-                    start_cell=cell_to_evaluate, end_cell=next_cell_to_evaluate))
-                # reset the astar algorithm
-                self._astar.reset()
+                # update the progress bar
+                pbar.update(last_cells_to_view - self.cells_to_view)
+
+                # pick the next cell to navigate to (closest frontier)
+                next_cell_to_evaluate = self._get_next_cell_to_evaluate(frontier_cells)
+                if next_cell_to_evaluate is None:
+                    break
+                cells_to_evaluate.append(next_cell_to_evaluate)
+                # with the freshly viewed cells, maybe some old frontier cells are no longer frontiers, revisit.
+                [frontier_cells.remove(cell) for cell in frontier_cells if not self._is_frontier(cell)]
                 cell_to_evaluate = next_cell_to_evaluate
                 # mark the cell you're going to navigate as empty, so the subsequent calls don't crash
                 self._grid[cell_to_evaluate] = EMPTY
                 self._cells_to_view = self._grid.get_empty_cells()
-                # with the freshly viewed cells, maybe some old frontier cells are no longer frontiers, revisit.
-                frontiers_to_delete = []
-                for cell in frontier_cells:
-                    if not self._is_frontier(cell):
-                        frontiers_to_delete.append(cell)
-                for cell in frontiers_to_delete:
-                    frontier_cells.remove(cell)
-        return ret
+        return cells_to_evaluate
 
     def _is_frontier(self, cell: tuple) -> bool:
         """
@@ -99,14 +98,41 @@ class BlueprintAwareFrontierExploration(object):
         lidar_range = self._range_in_cells if not frontier_eval else self._range_in_cells
         return raycast_in_every_direction(available_cells=available_cells, start_cell=cell, range_in_cells=lidar_range, squarify=True)
 
-    def plot(self):
-        return self._grid.plot()
-    
     def _raycast_some(self, cells: List[tuple]) -> None:
         for cell in cells:
             self._visibility_grid[cell] = self._raycast_in_every_direction(cell=cell, available_cells=self._cells_to_view)
         
 
+    @property
+    def cells_to_view(self) -> int:
+        return len(self._cells_to_view)
+
+    def _get_frontier_cells(self, cell_to_evaluate):
+        frontier_cells = []
+        # check which cells you can view from the current cell
+        raycasted_cells = self._raycast_in_every_direction(
+            cell=cell_to_evaluate, available_cells=self._cells_to_view)
+        # mark them as viewed, and check whether they're frontiers or not (frontier == boundary between explored and unexplored)
+        for cell in raycasted_cells:
+            self._grid[tuple(cell)] = VIEWED
+            if self._is_frontier(tuple(cell)):
+                frontier_cells.append(tuple(cell))
+        return frontier_cells
+
+    def _get_next_cell_to_evaluate(self, frontier_cells):
+        next_cell_to_evaluate = None
+        best_visibility = None
+            
+        self._raycast_some(cells=frontier_cells)
+        for cell in frontier_cells:
+            if best_visibility is None or len(self._visibility_grid[cell] > best_visibility):
+                next_cell_to_evaluate = cell
+                best_visibility = len(self._visibility_grid[cell])
+        return next_cell_to_evaluate
+
+
+    def plot(self, *args, **kwargs):
+        return self._grid.plot(*args, **kwargs)
         
 
 
