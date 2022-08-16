a0d536afdde81751fcb4ac1b733104590ec831a6
Ramiro Serra <serraramiro@ekumenlabs.com>
2022-08-16 09:39:32 -0300

    Progress on visibility node

--- src/ROS/create_autonomy/visibility_server/src/scripts/visibility_server_node.py
+++ src/ROS/create_autonomy/visibility_server/src/scripts/visibility_server_node.py
@@ -1,22 +1,27 @@
 #!/usr/bin/python3.8
-from typing import Any
+from typing import Tuple
 import FrontierExploration.preprocessing.grid.occupancy_grid as grid
 from FrontierExploration.preprocessing.grid.mock_grid import create_mock_grid
-from math import hypot
 from visibility_server.srv import Visibility, VisibilityRequest, VisibilityResponse
 
 import rospy
-# from sensor_msgs.msg import BatteryState
-from nav_msgs.msg import Odometry
-# from threading import Lock
-# from tf import TransformListener
+from dataclasses import dataclass, field
+
+
+
+@dataclass
+class InitialState:
+    initial_position: Tuple[int, int]
+    # TODO (implement) support starting misaligned with the grid
+    initial_rotation: Tuple [float, float, float, float] = field(default_factory=lambda: (1, 0, 0, 0))
 
 
 class VisibilityServer(object):
     """Consumvisibility_serveres time elapsed and robot odometry to estimate battery consumption
     """
-    def __init__(self, grid: grid.OccupancyGrid):
+    def __init__(self, grid: grid.OccupancyGrid, initial_state: InitialState):
         self._grid = grid
+        self._inital_state = initial_state
         self._service_server =  rospy.Service('visibility', Visibility, self._visibility_cb)
     
     
@@ -24,11 +29,13 @@ class VisibilityServer(object):
         """
         returns visibility of the cell corresponding to the given position in the map frame
         """
-        coordinate = (int(req.x_map_frame / self._grid.get_cell_size()), int(req.y_map_frame / self._grid.get_cell_size()))
+        coordinate_map_frame = (int(req.x_map_frame / self._grid.get_cell_size()), int(req.y_map_frame / self._grid.get_cell_size()))
+        # substract initial offset 
+        # (go from map frame to grid frame)
+        coordinate_grid_frame = (coordinate_map_frame[0] - self._inital_state.initial_position[0], coordinate_map_frame[1] - self._inital_state.initial_position[1])
         response = VisibilityResponse()
         try:
-
-            response.visibility =  grid[coordinate]
+            response.visibility =  grid[coordinate_grid_frame]
         except IndexError:
             response.visibility = 0
         return response
@@ -46,9 +53,11 @@ class VisibilityServer(object):
     
 if __name__ == '__main__':
     rospy.init_node('VisibilityServer', anonymous=False, log_level=rospy.DEBUG)
+    # TODO expose parameters via parameter server
     try:
+        initial_state = InitialState(initial_position=(0, 0))
         grid = create_mock_grid(10, 10, 1, 100)
-        visibility_server = VisibilityServer(grid=grid)
+        visibility_server = VisibilityServer(grid=grid, initial_state=initial_state)
         visibility_server.run()
     except rospy.ROSInterruptException:
         pass
