50e510d324ec54990010c3fc428472f258ff6cb9
agusmazzeo1 <agusmazzeo1@gmail.com>
2022-12-15 19:38:01 -0300

    syntetic external polygon

--- src/FrontierExploration/preprocessing/layout/syntetic.py
+++ src/FrontierExploration/preprocessing/layout/syntetic.py
@@ -92,7 +92,8 @@ class SynteticWorld:
         x_room_range: int = 50,
         y_room_range: int = 50,
         wall_thickness: float = 0.1,
-        wall_height: float = 5
+        wall_height: float = 5,
+        external_polygon: Polygon = None
     ):    
         self.world_name = name
         self.n_rectangles = n_rectangles
@@ -108,7 +109,7 @@ class SynteticWorld:
         makedirs(self.export_models_dir, exist_ok=True)
         makedirs(self.export_world_dir, exist_ok=True)
         makedirs(self.export_polygons_dir, exist_ok=True)
-        self.wall_polygon = self.create_polygon()
+        self.wall_polygon = self.create_polygon(external_polygon)
         self._starting_point = None
         # Create a world generator to place
         # objects in the world
@@ -118,63 +119,65 @@ class SynteticWorld:
         self.n_cylinders = None
         self.n_spheres = None
 
-    def create_polygon(self):
+    def create_polygon(self, external_polygon: Polygon =None):
         # Generate the reference polygon for the wall boundaries
-        if self.n_rectangles is not None:
-            if self.n_rectangles > 1:
-                wall_polygon = random_rectangles(
-                    n_rect=self.n_rectangles,
-                    x_center_min=-self.x_room_range,
-                    x_center_max=self.x_room_range,
-                    y_center_min=-self.y_room_range,
-                    y_center_max=self.y_room_range,
-                    delta_x_min=6,
-                    delta_x_max=12,
-                    delta_y_min=6,
-                    delta_y_max=12)
-            elif self.n_rectangles == 1:
-                wall_polygon = random_rectangle(
-                    delta_x_min=self.x_room_range / 2.,
-                    delta_x_max=self.x_room_range,
-                    delta_y_min=self.y_room_range / 2.,
-                    delta_y_max=self.y_room_range)
+        if external_polygon is None:
+            if self.n_rectangles is not None:
+                if self.n_rectangles > 1:
+                    wall_polygon = random_rectangles(
+                        n_rect=self.n_rectangles,
+                        x_center_min=-self.x_room_range,
+                        x_center_max=self.x_room_range,
+                        y_center_min=-self.y_room_range,
+                        y_center_max=self.y_room_range,
+                        delta_x_min=6,
+                        delta_x_max=12,
+                        delta_y_min=6,
+                        delta_y_max=12)
+                elif self.n_rectangles == 1:
+                    wall_polygon = random_rectangle(
+                        delta_x_min=self.x_room_range / 2.,
+                        delta_x_max=self.x_room_range,
+                        delta_y_min=self.y_room_range / 2.,
+                        delta_y_max=self.y_room_range)
+                else:
+                    raise ValueError(
+                        'Number of rectangles is invalid, provided={}'.format(
+                        self.n_rectangles))
+            elif self.n_points is not None:
+                if self.n_points < 3:
+                    raise ValueError(
+                        'Number of points for triangulation must be at least 3,'
+                        ' provided={}'.format(self.n_points))
+                wall_polygon = random_points_to_triangulation(
+                    self.n_points)
             else:
                 raise ValueError(
-                    'Number of rectangles is invalid, provided={}'.format(
-                       self.n_rectangles))
-        elif self.n_points is not None:
-            if self.n_points < 3:
-                raise ValueError(
-                    'Number of points for triangulation must be at least 3,'
-                    ' provided={}'.format(self.n_points))
-            wall_polygon = random_points_to_triangulation(
-                self.n_points)
+                    'No number of rectangles and no number of points for'
+                    ' triangulation were provided')
+            if self.n_internal_rectangles is not None and self.n_internal_rectangles > 0:
+                min_x, min_y, max_x, max_y = wall_polygon.bounds
+                width = max_x - min_x
+                height = max_y - min_y
+                MAX_ATTEMPTS = 100
+                for _ in range(self.n_internal_rectangles):
+                    internal_polygon = None
+                    attempts = 0
+                    while attempts < MAX_ATTEMPTS and (internal_polygon is None or not wall_polygon.contains(internal_polygon)):
+                        attempts+=1
+                        rand_x = random.uniform(min_x/10, max_x/10)
+                        rand_y = random.uniform(min_y/10, max_y/10)
+
+                        left = Point([rand_x, rand_y])
+                        bottom = Point([rand_x, rand_y - height])
+                        right = Point([rand_x + width, rand_y - height])
+                        top = Point([rand_x + width, rand_y])
+
+                        internal_polygon = Polygon([left, bottom, right, top])
+                        
+                    wall_polygon = wall_polygon.difference(internal_polygon)
         else:
-            raise ValueError(
-                'No number of rectangles and no number of points for'
-                ' triangulation were provided')
-        if self.n_internal_rectangles is not None and self.n_internal_rectangles > 0:
-            min_x, min_y, max_x, max_y = wall_polygon.bounds
-            width = max_x - min_x
-            height = max_y - min_y
-            MAX_ATTEMPTS = 100
-            for _ in range(self.n_internal_rectangles):
-                internal_polygon = None
-                attempts = 0
-                while attempts < MAX_ATTEMPTS and (internal_polygon is None or not wall_polygon.contains(internal_polygon)):
-                    attempts+=1
-                    rand_x = random.uniform(min_x/10, max_x/10)
-                    rand_y = random.uniform(min_y/10, max_y/10)
-
-                    left = Point([rand_x, rand_y])
-                    bottom = Point([rand_x, rand_y - height])
-                    right = Point([rand_x + width, rand_y - height])
-                    top = Point([rand_x + width, rand_y])
-
-                    internal_polygon = Polygon([left, bottom, right, top])
-                    
-                wall_polygon = wall_polygon.difference(internal_polygon)
-
+            wall_polygon = external_polygon
         tf_to_zero = [1, 0, 0, 1, -wall_polygon.bounds[0], -wall_polygon.bounds[1]]
         wall_polygon = affine_transform(wall_polygon, tf_to_zero)
         
@@ -201,9 +204,10 @@ class SynteticWorld:
             pose=[centroid.x, centroid.y, self.wall_height / 2., 0, 0, 0],
             extrude_boundaries=True,
             color='xkcd')
-        walls_model.name = self.world_name + '_walls'
+        walls_model.name = self.world_name + '_wall'
 
-
+        print(f"Extruded walls")
+        print(f"{walls_model.name}")
 
         # Add walls and ground plane to the world
         self.world_generator.world.add_model(
@@ -212,120 +216,120 @@ class SynteticWorld:
         self.world_generator.world.add_model(
             tag='ground_plane',
             model=SimulationModel.from_gazebo_model('ground_plane'))
+        # print("Started free space method")
+        # # Retrieve the free space polygon where objects
+        # # can be placed within the walls
+        # free_space_polygon = self.world_generator.world.get_free_space_polygon(
+        #     ground_plane_models=[walls_model.name],
+        #     ignore_models=['ground_plane'])
+        # print(f"Got free space polygon")
+        # # Add the workspace constraint to the
+        # # generator
+        # self.world_generator.add_constraint(
+        #     name='room_workspace',
+        #     type='workspace',
+        #     frame='world',
+        #     geometry_type='polygon',
+        #     polygon=free_space_polygon
+        # )
 
-        # Retrieve the free space polygon where objects
-        # can be placed within the walls
-        free_space_polygon = self.world_generator.world.get_free_space_polygon(
-            ground_plane_models=[walls_model.name],
-            ignore_models=['ground_plane'])
+        # # Add constraint to place all object
+        # # tangent to the ground
+        # self.world_generator.add_constraint(
+        #     name='tangent_to_ground_plane',
+        #     type='tangent',
+        #     frame='world',
+        #     reference=dict(
+        #         type='plane',
+        #         args=dict(
+        #             origin=[0, 0, 0],
+        #             normal=[0, 0, 1]
+        #         )
+        #     )
+        # )
 
-        # Add the workspace constraint to the
-        # generator
-        self.world_generator.add_constraint(
-            name='room_workspace',
-            type='workspace',
-            frame='world',
-            geometry_type='polygon',
-            polygon=free_space_polygon
-        )
-
-        # Add constraint to place all object
-        # tangent to the ground
-        self.world_generator.add_constraint(
-            name='tangent_to_ground_plane',
-            type='tangent',
-            frame='world',
-            reference=dict(
-                type='plane',
-                args=dict(
-                    origin=[0, 0, 0],
-                    normal=[0, 0, 1]
-                )
-            )
-        )
-
-        # Add assets
-        models = dict()
-        if self.n_cubes is not None and self.n_cubes > 0:
-            self.world_generator.add_asset(
-                tag='box',
-                description=dict(
-                    type='box',
-                    args=dict(
-                        size=f"__import__('numpy').random.uniform(0.1, {cube_size}, 3)",
-                        name='cuboid',
-                        color='xkcd'
-                    )
-                )
-            )
-            models['box'] = self.n_cubes
+        # # Add assets
+        # models = dict()
+        # if self.n_cubes is not None and self.n_cubes > 0:
+        #     self.world_generator.add_asset(
+        #         tag='box',
+        #         description=dict(
+        #             type='box',
+        #             args=dict(
+        #                 size=f"__import__('numpy').random.uniform(0.1, {cube_size}, 3)",
+        #                 name='cuboid',
+        #                 color='xkcd'
+        #             )
+        #         )
+        #     )
+        #     models['box'] = self.n_cubes
 
 
-        orientation_dofs = ['yaw']
+        # orientation_dofs = ['yaw']
 
-        # Add placement policy
-        placement_policy = dict(
-            models=list(models.keys()),
-            config=[
-                dict(
-                    dofs=['x', 'y'],
-                    tag='workspace',
-                    workspace='room_workspace'
-                ),
-                dict(
-                    dofs=orientation_dofs,
-                    tag='uniform',
-                    mean=0,
-                    min=-3.141592653589793,
-                    max=3.141592653589793
-                )
-            ]
-        )
+        # # Add placement policy
+        # placement_policy = dict(
+        #     models=list(models.keys()),
+        #     config=[
+        #         dict(
+        #             dofs=['x', 'y'],
+        #             tag='workspace',
+        #             workspace='room_workspace'
+        #         ),
+        #         dict(
+        #             dofs=orientation_dofs,
+        #             tag='uniform',
+        #             mean=0,
+        #             min=-3.141592653589793,
+        #             max=3.141592653589793
+        #         )
+        #     ]
+        # )
 
-        # Set local constraints
-        local_constraints = list()
-        for m in models:
-            local_constraints.append(
-                dict(model=m, constraint='tangent_to_ground_plane'))
+        # # Set local constraints
+        # local_constraints = list()
+        # for m in models:
+        #     local_constraints.append(
+        #         dict(model=m, constraint='tangent_to_ground_plane'))
 
-        # Place objects randomly on the free
-        # space within the walls
-        self.world_generator.add_engine(
-            tag='placement_engine',
-            engine_name='random_pose',
-            models=list(models.keys()),
-            max_num=models,
-            model_picker='random',
-            no_collision=True,
-            min_distance=0.0,
-            policies=[placement_policy],
-            constraints=local_constraints
-        )
+        # # Place objects randomly on the free
+        # # space within the walls
+        # self.world_generator.add_engine(
+        #     tag='placement_engine',
+        #     engine_name='random_pose',
+        #     models=list(models.keys()),
+        #     max_num=models,
+        #     model_picker='random',
+        #     no_collision=True,
+        #     min_distance=0.0,
+        #     policies=[placement_policy],
+        #     constraints=local_constraints
+        # )
 
-        # Run placement engine
-        self.world_generator.run_engines(attach_models=True)
-        self.free_space_polygon = self.world_generator.world.get_free_space_polygon(
-            ground_plane_models=self.world_generator.world.models.keys(),
-            ignore_models=['ground_plane'])
-        self.free_space_polygon = self.free_space_polygon.intersection(self.wall_polygon)
-        _ = self.get_starting_point()
-        self.world_generator.world.name = self.world_name
-        if show:
-            self.world_generator.world.show()
+        # # Run placement engine
+        # self.world_generator.run_engines(attach_models=True)
+        # self.free_space_polygon = self.world_generator.world.get_free_space_polygon(
+        #     ground_plane_models=self.world_generator.world.models.keys(),
+        #     ignore_models=['ground_plane'])
+        # self.free_space_polygon = self.free_space_polygon.intersection(self.wall_polygon)
+        # _ = self.get_starting_point()
+        # self.world_generator.world.name = self.world_name
+        # if show:
+        #     self.world_generator.world.show()
 
-        add_custom_gazebo_resource_path(self.export_models_dir)
+        # add_custom_gazebo_resource_path(self.export_models_dir)
 
-        # Export world to file and walls model as Gazebo model
-        full_world_filename = self.world_generator.export_world(
-            output_dir=self.export_world_dir,
-            filename=self.world_generator.world.name + '.world',
-            models_output_dir=self.export_models_dir,
-            overwrite=True)
-        # Export walls polygon to file.
-        polygon_filename = self.world_generator.world.name + '_polygon.pkl'
+        # # Export world to file and walls model as Gazebo model
+        # full_world_filename = self.world_generator.export_world(
+        #     output_dir=self.export_world_dir,
+        #     filename=self.world_generator.world.name + '.world',
+        #     models_output_dir=self.export_models_dir,
+        #     overwrite=True)
+        # # Export walls polygon to file.
+        # polygon_filename = self.world_generator.world.name + '_polygon.pkl'
 
-        with open(os.path.join(self.export_polygons_dir, polygon_filename), "wb") as fp:
-            pickle.dump(obj=self.wall_polygon, file=fp)
+        # with open(os.path.join(self.export_polygons_dir, polygon_filename), "wb") as fp:
+        #     pickle.dump(obj=self.wall_polygon, file=fp)
 
     @property
     def total_area(self):
